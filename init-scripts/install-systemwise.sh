48899c94bff0253b371782c29f9bd52e57bb0110
JRozen <thequetran@gmail.com>
2020-10-01 22:06:46 +0200

    update install systemwise script

--- init-scripts/install-systemwise.sh
+++ init-scripts/install-systemwise.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 DIR="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-install() {
-  if ! diff -N --color "$1" "$DIR$1"; then
+install_systemwise() {
+  src="$DIR/root$1"
+  if ! diff -N --color "$1" "$src"; then
     printf "\n"
     echo "Found changes while installing $1"
-    read -r -p "Is this change ok? [y/N] " response
+    read -r -p "Is this change ok? [y/N] " response </dev/tty
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
     then
-        sudo cp -v "$DIR$1"  "$1"
+        sudo cp -v "$src"  "$1"
         printf "\n\n"
     fi
   fi
 }
 
-install /usr/bin/blurlock
-install /usr/bin/lockscreen
-install /usr/bin/i3exit
-install /usr/local/bin/xflock4
+find "$DIR/root" -type f | awk -F'root' '{ print $2}' |
+while read location; do
+    install_systemwise "$location";
+done;
+
