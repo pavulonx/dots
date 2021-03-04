230c79f3773ceb42fc515587527c623dee5de271
jrozen <jrozen@tuta.io>
2021-03-04 23:54:50 +0100

    simplify polybar

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-# Terminate already running bar instances
 killall -q polybar
-
-echo "$DESKTOP_SESSION" >> ~/polybar_dbg
 
 case $DESKTOP_SESSION in
 	bspwm)  bar=bspwm ;;
@@ -14,17 +11,6 @@ esac
 
 [ -z "$bar" ] && exit 1
 
-if type "xrandr"; then
-  for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload "$bar" &
-  done
-else
-  polybar --reload "$bar" &
-fi
-
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
-
-# Launch bars
-polybar "$bar" &
-
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+  MONITOR=$m polybar --reload "$bar" &
+done
