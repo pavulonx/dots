e168225cc22eadc14bf28cae6bfad4b2e4671a78
jrozen <jrozen@tuta.io>
2021-05-16 12:54:31 +0200

    lockscreen cleanup

--- extras/bootstrap/systemcustom.sh
+++ extras/bootstrap/systemcustom.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DIR="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
+changed=false
 install_systemwise() {
   src="$DIR/$1$2"
   if ! diff -N --color "$2" "$src"; then
@@ -8,8 +9,9 @@ install_systemwise() {
     echo "Found changes while installing $2"
     read -r -p "Is this change ok? [y/N] " response </dev/tty
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-        sudo cp -v "$src"  "$2"
-        printf "\n\n"
+      sudo cp -v "$src" "$2"
+      changed=true
+      printf "\n\n"
     fi
   fi
 }
@@ -17,8 +19,9 @@ install_systemwise() {
 tree_dir=tree
 
 find "$DIR/$tree_dir" -type f | awk -F"$tree_dir" '{ print $2}' |
-while read -r location; do
+  while read -r location; do
     install_systemwise "$tree_dir" "$location";
-done;
+  done;
 
-echo "Done!"
+  $changed && echo "Changes applied!"
+  exit 0
