68d5a4fbfbfa10c4d74f7aa55f13ba9e20034c01
jr <pavulon@tuta.io>
2021-08-05 22:24:05 +0200

    replace alacritty with /usr/bin/terminal, cleanup

--- dot_config/xmonad/xmonad.hs
+++ dot_config/xmonad/xmonad.hs
@@ -169,7 +169,7 @@ myModMask :: KeyMask
 myModMask = mod4Mask       -- Sets modkey to super/windows key
 
 myTerminal :: String
-myTerminal = "alacritty"   -- Sets default terminal
+myTerminal = "/usr/bin/terminal"   -- Sets default terminal
 
 myBorderWidth :: Dimension
 myBorderWidth = 1          -- Sets border width for windows
