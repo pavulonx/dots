e8f9629bc094c80e0f824c6cdbca8e9564bef2c8
jr <pavulon@tuta.io>
2021-08-08 12:18:57 +0200

    update limine-gen

--- extras/scripts/limine-gen.sh
+++ extras/scripts/limine-gen.sh
@@ -1,30 +1,27 @@
 #!/bin/sh
 
-# support for fallback initramfs
-
-limine_cmdline="${limine_cmdline:-}"
+kernel_cmdline="${LIMINE_LINUX_CMDLINE:-}"
 
 os_name="$(grep -E '^NAME=' /etc/os-release | cut -d'"' -f2)"
 hostname="$(cat /etc/hostname)"
 
-header="# /boot/limine.cfg - limine configuration file
-# OS: $os_name
-# hostname: $hostname
-
+header="\
 DEFAULT_ENTRY=1
 TIMEOUT=5
-VERBOSE=yes"
+VERBOSE=yes
+"
 normal_entries=""
 fallback_entries=""
 
+# parse entries
 for _kernel in $(find /boot -name 'vmlinuz-*' | sort -u); do
-  _kn="${_kernel#*vmlinuz-}"
-  _kv="${_kn#linux}"; _kv="${_kv#-}"; _kv="${_kv:+ - $_kv}"
+  _kn="${_kernel#*vmlinuz-}";
+  _kv="${_kn#linux}"; _kv="${_kv#-}"; _kv="${_kv%-*}"; _kv="${_kv:+ - $_kv}";
   entry="\
 PROTOCOL=linux
 KERNEL_PATH=boot:///vmlinuz-$_kn"
-  [ -n "$limine_cmdline" ] && entry="$entry
-CMDLINE=$limine_cmdline"
+  [ -n "$kernel_cmdline" ] && entry="$entry
+CMDLINE=$kernel_cmdline"
   [ -e "/boot/intel-ucode.img" ] && entry="$entry
 MODULE_PATH=boot:///intel-ucode.img"
 
@@ -38,12 +35,17 @@ MODULE_PATH=boot:///initramfs-$_kn.img
 $entry
 MODULE_PATH=boot:///initramfs-$_kn-fallback.img
 "
-  unset entry
   unset _kn
   unset _kv
+  unset entry
 done
 
-echo "$header
+echo "\
+# /boot/limine.cfg - limine configuration file
+# OS: $os_name
+# hostname: $hostname
+
+$header
 
 $normal_entries
 
