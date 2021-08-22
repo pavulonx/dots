dea3d85b50d20a96b2092b13e13f45fac58e6501
jr <pavulon@tuta.io>
2021-08-22 19:33:49 +0200

    install packages script update

--- extras/scripts/install-dependencies.sh
+++ extras/scripts/install-dependencies.sh
@@ -1,197 +1,215 @@
-#!/bin/bash
-#set -e
+#!/usr/bin/env bash
 
-install_all() {
-	echo "$*" | xargs paru -S --needed
+set -euo pipefail
+
+_install() {
+  paru -S --needed "$@"
 }
 
+_log_install() {
+  pad='─────────────────────────────────────────────────────────────────────────────'
+  num="$*"; num="${#num}"
+  sep="${pad:0:${num}}"
+  echo ''
+  echo "$sep"
+  printf '\e[0;32;1m%s\e[0m\n' "$*"
+  echo "$sep"
+}
+
+if readlink /sbin/init 2>/dev/null | grep -q openrc; then
+  initsys=openrc
+elif grep -q 'systemd' /proc/1/comm; then
+  initsys=systemd
+fi
+
+_log_install "Installing core"
 core=(
-bat
-chezmoi
-git-delta
-dust
-exa
-fd
-fzf
-git
-htop
-jq
-lf-bin
-lua
-moreutils
-neofetch
-neovim
-openssh
-pacman-contrib
-polkit
-psmisc
-ranger
-ripgrep
-shellcheck
-tealdeer
-tree
-tumbler
-unzip
-zip
-zsh
-zsh-completions
+  bat
+  chezmoi
+  git-delta
+  dust
+  exa
+  fd
+  fzf
+  git
+  htop
+  jq
+  lf-bin
+  lua
+  moreutils
+  neofetch
+  neovim
+  openssh
+  pacman-contrib
+  polkit
+  psmisc
+  ranger
+  ripgrep
+  shellcheck
+  tealdeer
+  tree
+  tumbler
+  unzip
+  zip
+  zsh
+  zsh-completions
 )
-echo "Installing core"
-install_all "${core[@]}"
+_install "${core[@]}"
 
 
+_log_install "Installing minimal gui"
 gui=(
-adwaita-icon-theme
-arandr
-autorandr
-clipmenu
-dunst
-feh
-flatery-icon-theme-git
-gsimplecal
-i3-gaps
-i3lock-color-git
-lxappearance
-maim
-mate-polkit
-networkmanager_dmenu
-picom
-polybar
-redshift
-rofi
-scrot
-thunar
-thunar-archive-plugin
-thunar-volmanudiskie
-ueberzug
-unclutter
-xautomation
-xbindkeys
-xclip
-xdg-user-dirs
-xdg-utils
-xdotool
-xmonad
-xmonad-contrib
-xmonad-utils
-xorg-xkill
-xsel
-xss-lock
+  adwaita-icon-theme
+  arandr
+  autorandr
+  clipmenu
+  dunst
+  feh
+  flatery-icon-theme-git
+  gsimplecal
+  i3-gaps
+  i3lock-color-git
+  lxappearance
+  maim
+  mate-polkit
+  networkmanager-dmenu-git
+  picom
+  polybar
+  redshift
+  rofi
+  scrot
+  thunar
+  thunar-archive-plugin
+  thunar-volman
+  udiskie
+  ueberzug
+  unclutter
+  xautomation
+  xbindkeys
+  xclip
+  xdg-utils
+  xdotool
+  xmonad
+  xmonad-contrib
+  xmonad-utils
+  xorg-xkill
+  xsel
+  xss-lock
 )
-echo "Installing minimal gui"
-install_all "${gui[@]}"
+_install "${gui[@]}"
 
 
+_log_install "Installing sound packages"
 sound=(
-pamixer
-pavucontrol
-pipewire
-pipewire-media-session
-pipewire-pulse
-playerctl
+  pamixer
+  pavucontrol
+  pipewire
+  pipewire-media-session
+  pipewire-pulse
+  playerctl
 )
-echo "Installing sound packages"
-install_all "${sound[@]}"
+_install "${sound[@]}"
 
 
-printer=(
-cups
-cups-pdf
-ghostscript
-gsfonts
-libcups
-system-config-printer
-)
-echo "Installing printer packages"
-install_all "${printer[@]}"
 
 
+_log_install "Installing fonts packages"
 fonts=(
-adobe-source-sans-pro-fonts
-awesome-terminal-fonts
-cantarell-fonts
-nerd-fonts-complete
-noto-fonts
-noto-fonts-emoji
-noto-fonts-extra
-ttf-droid
-ttf-font-awesome
-ttf-hack
-ttf-inconsolata
-ttf-jetbrains-mono
-ttf-lato
-ttf-liberation
-ttf-roboto
+  adobe-source-sans-pro-fonts
+  awesome-terminal-fonts
+  cantarell-fonts
+  nerd-fonts-complete
+  noto-fonts
+  noto-fonts-emoji
+  noto-fonts-extra
+  ttf-droid
+  ttf-font-awesome
+  ttf-hack
+  ttf-inconsolata
+  ttf-jetbrains-mono
+  ttf-lato
+  ttf-liberation
+  ttf-roboto
 )
-echo "Installing fonts packages"
-install_all "${fonts[@]}"
+_install "${fonts[@]}"
 
 
 programs=(
-brave-beta-bin
-evince
-file-roller
-firefox-developer-edition
-gimp
-gnome-calculator
-gparted
-gpick
-gucharmap
-mpv
-peek
-solaar-git
-spotify
-sxiv
-system-config-printer
-transmission-cli
-zathura
+  brave-beta-bin
+  file-roller
+  firefox-developer-edition
+  gimp
+  gnome-calculator
+  gparted
+  gpick
+  gucharmap
+  mpv
+  peek
+  solaar-git
+  sxiv
+  transmission-cli
+  youtube-dl
+  zathura
 )
-echo "Installing programs packages"
-install_all "${programs[@]}"
+_log_install "Installing programs packages"
+_install "${programs[@]}"
 
 
-read -p "Install laptop dependencies? [y/N] " -n 1 -r
+
+printf "\nInstall printers packages? [y/N]: "
+read -r REPLY
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  laptop=(
-  tlp
-  )
-
-  echo "Installing laptop packages"
-  install_all "${laptop[@]}"
-
-  echo "Post install inits"
-
-  echo "Enabling tlp.service"
-  sudo systemctl enable tlp.service
-
+  _log_install "Installing printer packages"
+  printer=(cups cups-pdf ghostscript gsfonts libcups system-config-printer)
+  _install "${printer[@]}"
 fi
 
-echo "LAPTOP"
 
-read -p "Install bluetooth dependencies? [y/N] " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]; then
 
-  bluetooth=(
-  pulseaudio-bluetooth
-  bluez
-  bluez-libs
-  bluez-utils
-  )
+printf "\nInstall laptop dependencies? [y/N]: "
+read -r REPLY
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+  laptop=(tlp tlpui acpi acpid acpi_call)
 
-  echo "Installing bluetooth packages"
-  install_all "${bluetooth[@]}"
+  _log_install "Installing laptop packages"
+  _install "${laptop[@]}"
 
-  echo "Post install inits"
-
-  echo "Enabling bluetooth.service"
-  sudo systemctl enable bluetooth.service
-  sudo systemctl start bluetooth.service
-  sudo sed -i 's/'#AutoEnable=false'/'AutoEnable=true'/g' /etc/bluetooth/main.conf
-
+  if [[ "$initsys" == "openrc" ]]; then
+    _install "tlp-openrc"
+    echo "You can now enable tlp-openrc service"
+  elif [[ "$initsys" == "systemd" ]]; then
+    echo "Enabling tlp.service"
+    sudo systemctl enable tlp.service
+  fi
+  echo "See https://wiki.archlinux.org/title/TLP for further configuration"
+  echo " (e.g. ThinkPad specific battery settings)"
 fi
 
-printf "\n\nCOMPLETED!\n\n"
 
-exit 0
-#installation for openrc
-#openrc-zsh-completions
+printf "\ninstall bluetooth packages? [y/N]: "
+read -r reply
+if [[ "$reply" =~ ^[yy]$ ]]; then
+
+  bluetooth=(bluez bluez-libs bluez-utils blueman)
+  _log_install "Installing bluetooth"
+  _install "${bluetooth[@]}"
+
+  echo "Post install inits"
+  if [[ "$initsys" == "openrc" ]]; then
+    echo "You can now enable bluetooth services service in openrc"
+  elif [[ "$initsys" == "systemd" ]]; then
+    echo "Enabling bluetooth.service"
+    sudo systemctl enable bluetooth.service
+    sudo systemctl start bluetooth.service
+    sudo sed -i 's/'#AutoEnable=false'/'AutoEnable=true'/g' /etc/bluetooth/main.conf
+  fi
+fi
+
+
+if [[ "$initsys" == "openrc" ]]; then
+  openrc=(openrc-zsh-completions)
+  _log_install "Installing openrc addons"
+  _install "${openrc[@]}"
+fi
+
+printf "\nInstalation complete!\n"
